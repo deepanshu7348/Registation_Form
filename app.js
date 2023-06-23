d90ebd1576c8f8910d9690541cf1d72a62d10e95
Deepanshu7348 <108023848+Deepanshu7348@users.noreply.github.com>
2023-06-23 21:39:12 +0530

    registation

---
+++ app.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const encrypt = require('mongoose-encryption');
+const ejs = require("ejs");
+
+var app = express();
+const port = process.env.PORT || 5000; 
+app.set("view engine","ejs");
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+
+
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/secrets");
+
+const trySchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+
+
+//data encryption
+const secret = "thisislittlesecret";
+trySchema.plugin(encrypt,{secret:secret,encryptedFields:["password"]});
+
+const item = mongoose.model("second",trySchema);
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.post("/register", function(req, res){
+    const newUser = new item({
+        email: req.body.username,     
+        password: req.body.password
+    });
+    newUser.save().then(()=>{
+        res.render("secrets");
+    }).catch((err)=>{
+        console.log(err);
+    })
+});
+
+
+app.post("/login", function(req,res){
+    const username = req.body.username;
+    const password = req.body.password;
+    item.findOne({email:username})
+        .then((foundUser)=>{
+            if(foundUser){
+                if(foundUser.password == password){
+                    res.render("secrets");
+                }
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+
+        });
+});
+
+app.post("/submit",function(){
+    // const newData = new item({
+    //     textdata: req.body.textdata 
+    // });
+    // newData.save().then(()=>{
+    //     res.render("submit"); 
+    // }).catch((err)=>{
+    //     console.log(err);
+    // })
+    const textdata = req.body.textdata
+    item.findOne({email:username})
+        .then((foundUser)=>{
+            if(foundUser){
+                if(foundUser.password == password){
+                    res.render("submit");
+                }
+            }
+        })
+        .catch((err)=>{
+            console.log(err);
+
+        });
+});
+
+
+
+// app.post("/login", function(req,res){
+//     const username = req.body.username;
+//     const password = req.body.password;
+//     item.findOne({email:username},function(err,foundUser){
+//         if(err){
+//             console.log(err);
+//         }
+//         else{
+//             if(foundUser){
+//                 if(foundUser.password == password){
+//                     res.render("secrets");
+//                 }
+//             }
+//         }
+//     });
+// });
+
+
+app.get("/login",function(req,res){
+    res.render("login");
+});
+
+app.get("/register",function(req,res){
+    res.render("register");
+});
+app.get("/secrets",function(req,res){
+    res.render("secrets");
+});
+app.get("/submit",function(req,res){
+    res.render("submit");
+});
+
+app.listen(port,function(){
+    console.log(`server started on port ${port}`);
+});
+
+
+//vercel
